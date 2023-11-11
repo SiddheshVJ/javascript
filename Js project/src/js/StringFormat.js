7f8636d1b64eea1f0c78adeb3304cd507a83514b
Siddhesh <siddheshvjadhav20@gmail.com>
2023-11-11 12:56:36 +0530

    string manipulation methods

--- Js project/src/js/StringFormat.js
+++ Js project/src/js/StringFormat.js
@@ -1,2 +1,13 @@
 //  String formatting methods
- 
+
+
+const str = 'Welcome';
+
+console.log(str.charAt(15)) // won't return anything  return type void type
+console.log(str.charAt(5)) // return m
+
+console.log(str.charCodeAt(5)) // ASCII value of m = 109
+
+console.log(str.indexOf('e')) //  returns first occurrence i.e. 1
+console.log(str.lastIndexOf('e')) //  returns last occurrence i.e. 6
+console.log(str.indexOf('b')) //  returns -1
