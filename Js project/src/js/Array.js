ef2641a92b2f83739f3b8965daa11e10519d5d6b
Siddhesh <siddheshvjadhav20@gmail.com>
2023-11-13 19:53:56 +0530

    non primitive datatype

---
+++ Js project/src/js/Array.js
@@ -0,0 +1,15 @@
+// Array
+
+
+let array1 = [10, "A", true]
+// methods
+
+console.log(array1.length)
+console.log(array1.toString())
+console.log(array1.join('=>'))
+console.log(array1.slice(1, 2))
+
+array1.map((val) => {
+    console.log(val)
+})
+
