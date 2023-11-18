c275a910b86be9fab26b702652c38739be73c404
Siddhesh <siddheshvjadhav20@gmail.com>
2023-11-18 10:31:21 +0530

    ArrayOfOjects

---
+++ Js project/src/js/objects.js
@@ -0,0 +1,12 @@
+let products = [
+    {
+        "productId": 1,
+        "price": 452.2,
+        "Name": "TV"
+    },
+    {
+        "productId": 2,
+        "price": 482.2,
+        "Name": "Mobile"
+    }
+]
